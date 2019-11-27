d42fe60911f18cab846bab1da7166a1f971b82e3
Artem Snisarenko <asn@adorsys.com.ua>
2019-11-27 18:01:08 +0200

    Explicit monitoring message serialization on the client

--- bg-monitoring-client/src/main/java/de/adorsys/bg/monitoring/client/rabbitmq/RabbitMonitoringClient.java
+++ bg-monitoring-client/src/main/java/de/adorsys/bg/monitoring/client/rabbitmq/RabbitMonitoringClient.java
@@ -1,8 +1,13 @@
 package de.adorsys.bg.monitoring.client.rabbitmq;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import de.adorsys.bg.monitoring.api.MonitoringMessage;
 import de.adorsys.bg.monitoring.client.MonitoringClient;
 import org.springframework.amqp.AmqpException;
+import org.springframework.amqp.core.MessageBuilder;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 
 import java.io.IOException;
@@ -10,6 +15,12 @@ import java.io.IOException;
 public class RabbitMonitoringClient implements MonitoringClient {
 
     private final RabbitTemplate template;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    {
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+    }
 
     public RabbitMonitoringClient(RabbitTemplate template) {
         this.template = template;
@@ -18,9 +29,14 @@ public class RabbitMonitoringClient implements MonitoringClient {
     @Override
     public void send(MonitoringMessage message) throws IOException {
         try {
-            template.convertAndSend(message);
+            MessageBuilder messageBuilder = MessageBuilder.withBody(serialize(message));
+            template.send(messageBuilder.build());
         } catch (AmqpException e) {
             throw new IOException(e);
         }
+    }
+
+    byte[] serialize(MonitoringMessage message) throws IOException {
+        return objectMapper.writeValueAsBytes(message);
     }
 }
---
+++ bg-monitoring-client/src/test/java/de/adorsys/bg/monitoring/client/rabbitmq/RabbitMonitoringClientTest.java
@@ -0,0 +1,26 @@
+package de.adorsys.bg.monitoring.client.rabbitmq;
+
+import de.adorsys.bg.monitoring.api.MonitoringMessage;
+import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class RabbitMonitoringClientTest {
+
+    private final RabbitMonitoringClient rabbitMonitoringClient = new RabbitMonitoringClient(null);
+
+    @Test
+    void nullPropertiesAreNotSerialized() throws Exception {
+        MonitoringMessage emptyMessage = MonitoringMessage.builder().build();
+        assertArrayEquals("{}".getBytes(), rabbitMonitoringClient.serialize(emptyMessage));
+    }
+
+    @Test
+    void timestampSerializedInIso8601Format() throws Exception {
+        MonitoringMessage message = MonitoringMessage.builder().setTimestamp(Instant.EPOCH).build();
+        byte[] actual = rabbitMonitoringClient.serialize(message);
+        assertArrayEquals("{\"timestamp\":\"1970-01-01T00:00:00Z\"}".getBytes(), actual);
+    }
+}
